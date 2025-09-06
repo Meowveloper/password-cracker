588304e40c39fb85e8d1060ca90c8f0e743afb3f
meowveloper <meowveloper.dev@gmail.com>
2025-09-07 01:10:49 +0700

    add 32 bit character string

--- src/dictionary_attack.c
+++ src/dictionary_attack.c
@@ -1,30 +1,34 @@
+#include "dictionary_attack.h"
+#include <openssl/md5.h>
 #include <stdio.h>
 #include <string.h>
-#include <openssl/md5.h>
-#include "dictionary_attack.h"
 
 void run_dictionary_attack(const char *hash, const char *wordlist_path) {
-    printf("dictionary attack initiated!!!\n");
+  printf("dictionary attack initiated!!!\n");
 
-    FILE *wordlist_file = fopen(wordlist_path, "r");
+  FILE *wordlist_file = fopen(wordlist_path, "r");
 
-    if(wordlist_file == NULL) {
-        printf("ERROR:: could not open file at the path %s\n", wordlist_path);
-        return;
+  if (wordlist_file == NULL) {
+    printf("ERROR:: could not open file at the path %s\n", wordlist_path);
+    return;
+  }
+  printf("Successfully opened file at %s\n", wordlist_path);
+
+  char line[256];
+  unsigned char md5_result[MD5_DIGEST_LENGTH];
+  char hex_hash[33];
+
+  while (fgets(line, sizeof(line), wordlist_file)) {
+    line[strcspn(line, "\n")] = '\0';
+    MD5((const unsigned char *)line, strlen(line), md5_result);
+
+    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+      sprintf(hex_hash + (i * 2), "%02x", md5_result[i]);
     }
-    printf("Successfully opened file at %s\n", wordlist_path);
+    printf("%s\n", hex_hash);
+  }
 
+  printf("\n");
 
-    char line[256];
-    unsigned char md5_result[MD5_DIGEST_LENGTH];
-
-    while (fgets(line, sizeof(line), wordlist_file))
-    {
-        line[strcspn(line, "\n")] = '\0';
-        MD5((const unsigned char *)line, strlen(line), md5_result);
-        printf("%s\n", line);
-    }
-    printf("\n"); 
-
-    fclose(wordlist_file);
+  fclose(wordlist_file);
 }
